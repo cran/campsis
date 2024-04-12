2c7be70f3dace62367dfbf2b98ed3f0edfc2cb54
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2024-04-12 09:00:09 +0000

    version 1.5.2

--- tests/testthat/testUtils.R
+++ tests/testthat/testUtils.R
@@ -7,7 +7,7 @@
 overwriteNonRegressionFiles <- FALSE
 testFolder <- ""
 skipLongTest <- TRUE
-skipVdiffrTest <- FALSE
+skipVdiffrTest <- TRUE
 testEngines <- c("rxode2", "mrgsolve")
 
 datasetInMemory <- function(dataset, model=NULL, seed, doseOnly=TRUE, settings, dest) {
